e079ea5d9dd884e811c37ad32f86834a17a01960
Martina Fedele <fedele.2000792@studenti.uniroma1.it>
2026-02-22 18:40:30 +0100

    Aggiunto modulo bare-metal per il controllo dei led e makefile dedicato
    Creazione della  cartella led che contiene la gestione dei led; Dettagli: -led.c: creazione del file led.c con all'interno la funzione led_on e led_off per la manipolazione diretta dei registri (PORTB), utilizzate anche mascherature bit a bit per modificare lo stato de singoli pin senza modificare gli altri.   -led.h: contiene la definizione dell'interfaccia pubblica del modulo e 'direttive' per prevenire inclusioni multiple (#ifndef,#define).  -Makefile:utile per testare solo la funzionalità dei led , contiene la configurazione completa della toolchain AVR,sono parametrizzati il target e la frequenza.

---
+++ led/led.c
@@ -0,0 +1,23 @@
+//Funzioni per il controllo hardware dei led tramite i registri AVR
+
+#define F_CPU 16000000UL  //definisco la frequenza di clock della CPU (16MHz nel caso di Maker uno)
+#include <stdio.h>
+#include <stdint.h>
+#include <util/delay.h>   //contiene le funzioni per i ritardi
+#include <avr/io.h>   //contiene la definizione dei registri hardware (PORTB,DDRB...)
+#include <avr/interrupt.h>  //contiene le librerie per gestire gli interrupt 8le ISR e la funzione sei())
+#include "led.h"
+
+
+//funzione che accende un led 
+//accende un pin senza modificare lo stato degli altri  utilizzando l'operatore logico bit a bit OR che confronta i bit attuali di PORTB con la maschera m
+void led_on(uint8_t m){
+    PORTB |= m; // accende il led senza toccare gli altri (se un bit nella maschera vale 1 il pin corrispondente in PORTB viene forzato a 1 e si accende, se vale 0 invece viene tenuto inalterato)
+}
+
+
+//funione che spegne un led
+//spegne un pin senza modificare lo stato degli altri
+void led_off(uint8_t m){
+    PORTB &= ~m;  // ~ inverte tutti i bit , uccido il pin dove vedo 0, &= prende lo stato attuale della porta e lo confronta con quello invertito , il risultato è 1 se entrambi sono 1 altrimenti è 0. Lascia tutto come è ma forzo solo il bit indicato da m
+}
---
+++ led/led.h
@@ -0,0 +1,11 @@
+#ifndef LED_H    // #indef, #define, #endif prevengono doppie inclusioni, se l'header viede chiamato da più file nello stesso progetto, il compilatore lo legge una sola volta evitando errori
+#define LED_H
+
+#include <stdint.h>
+
+
+// FUNZIONI PUBBLICHE
+void led_on(uint8_t m);   // funzione per accendere il led 
+void led_off(uint8_t m);  // funzione per spegnere il led
+
+#endif
